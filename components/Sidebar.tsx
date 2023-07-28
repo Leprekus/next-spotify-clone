bf2eedc32b56cff1bc9fc040e0c3fc4a81220054
Leprekus <leprekus@yahoo.com>
2023-07-28 08:31:28 -0700

    create routes & install icons

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,6 +1,22 @@
-import React, { type ReactNode } from 'react'
-
+'use client'
+import React, { useMemo, type ReactNode } from 'react'
+import { usePathname } from 'next/navigation'
 function Sidebar({ children }: { children : ReactNode }) {
+
+    const pathname = usePathname()
+    const routes = useMemo(() => [
+        {
+            label: 'Home',
+            active: pathname !== '/search',
+            href: '/'
+        },
+        {
+            label: 'Search',
+            active: pathname !== '/search',
+            href: '/search'
+        },
+    ], [])
+
   return (
     <div>{ children }</div>
   )
