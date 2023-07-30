20a0163c4a61f9989613c8efc9b0abfab336cfbc
Leprekus <leprekus@yahoo.com>
2023-07-29 22:15:05 -0700

    wrap app in clientProvider

--- providers/SupabaseProvider.tsx
+++ providers/SupabaseProvider.tsx
@@ -1,12 +1,18 @@
 'use client'
-import React, { type ReactNode } from 'react'
+import React, { useState, type ReactNode } from 'react'
 import { type Database } from '@/types_db'
-
+import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
+import { SessionContextProvider } from '@supabase/auth-helpers-react'
 interface SupabaseProviderProps {
     children : ReactNode
 }
 export default function SupabaseProvider({ children }: SupabaseProviderProps) {
-  return (
-    <div>SupabaseProvider</div>
+    const [ supabaseClient ] = useState(() => 
+        createClientComponentClient<Database>()
+    )
+    return (
+    <SessionContextProvider supabaseClient={supabaseClient}>
+        { children }
+    </SessionContextProvider>
   )
 }
