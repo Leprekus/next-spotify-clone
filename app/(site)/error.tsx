ed3dcd84e76a9e3fd59aad98d84ecbd20aea9c61
Leprekus <leprekus@yahoo.com>
2023-08-01 16:08:04 -0700

    move routes into (site)

---
+++ app/(site)/error.tsx
@@ -0,0 +1,19 @@
+'use client'
+
+import Box from '@/components/Box'
+import Button from '@/components/Button'
+import React from 'react'
+
+export default function Error() {
+
+  return (
+    <Box className='h-full flex flex-col gap-4 items-center justify-center'>
+        <div className='text-neutral-400'>
+            Something went wrong
+        </div>
+        <Button 
+        onClick={() => window.location.href = '/'}
+        className='w-24'>Refresh</Button>
+    </Box>
+  )
+}
